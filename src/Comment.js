72a66ecd98068117eadc74de82d887d440a42cfb
mattenbar <matthw.enbar@gmail.com>
2020-08-06 11:50:31 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
 //your code here
+import React, { Component } from 'react';
+
+export default class BlogContent extends Component {
+  render() {
+    return (
+      <div className="comment">
+        {this.props.commentText}
+      </div>  
+    )
+  }
+}
